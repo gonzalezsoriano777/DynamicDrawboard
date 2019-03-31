447e033ab7aa961ce460f55949b4cde975a5cf0f
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-03-31 16:53:09 -0500

    Unsure of the passing result.. Will have to check different command line

--- DynamicDrawboard/Program.cs
+++ DynamicDrawboard/Program.cs
@@ -11,31 +11,48 @@ namespace DynamicDrawboard
         char width = '|';
         string height = "++";
         string charspacing = " ";
-        
+
+
         static void Main(string[] args)
         {
             // Console.WriteLine("hello, world!!");
             Drawboard store = new Drawboard();
+            store.Board(6, 3);
         }
 
-        public void Board(int h, int w)
+
+        public void Board(int x, int y)
         {
             int i;
             int j;
 
-            // Rows
-            for (i = 0; i > w; i++)
+            // Rows, in width
+            for (i = 0; i > y; i++)
             {
-                
-            }
+                if(i % 2 == 0)
+                {
+                    Console.Write(" ");
+                } else
+                {
+                    Console.Write(width);
+                }
 
-            // Columns
-            for (j = 0; j < h; j++)
-            {
-                Console.Write(height);
+                for (j = 0; j < y; j++)
+                { 
+                    if(j % 2 == 0)
+                    {
+                        Console.Write(charspacing + width);
+                    }
+
+                    if(j % 2 == 1)
+                    {
+                        Console.Write(height);
+                    }
+                }
             }
 
             Console.ReadLine();
         }
+
     }
 }
